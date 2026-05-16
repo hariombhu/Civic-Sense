f0070e11065ffe3b5f4eba9a99eb6d18e2da6a5c
hariombhu <hariombhu01@gmail.com>
2026-05-16 17:56:33 +0530

    Create create_admin.py

---
+++ capstone-2-project-urban-Trace-main/create_admin.py
@@ -0,0 +1,13 @@
+import django
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'Admin@1234')
+    print('Superuser created successfully!')
+else:
+    print('Superuser already exists!')
