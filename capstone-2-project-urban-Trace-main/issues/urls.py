24a75794ba7237407114c5ec5a2a9203fa8b1b06
hariombhu <hariombhu01@gmail.com>
2026-05-16 19:51:39 +0530

    Update urls.py

--- capstone-2-project-urban-Trace-main/issues/urls.py
+++ capstone-2-project-urban-Trace-main/issues/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import IssueViewSet
 
 router = DefaultRouter()
-router.register(r'issues', IssueViewSet)
+router.register(r'issues', IssueViewSet, basename='issues')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]
